39e7f4c4f268f56d1418847f0ec9e4cbc6a97a0b
evhenk <ekiriyanov.pro@gmail.com>
2025-05-19 12:47:06 -0400

    Fix: Chrome config for CI/CD

--- conftest.py
+++ conftest.py
@@ -7,10 +7,14 @@ from webdriver_manager.chrome import ChromeDriverManager
 @pytest.fixture
 def driver():
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless=new")
+    options.add_argument("--headless=new")  # Для GitHub Actions
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-dev-tools")
+    options.add_argument("--disable-extensions")
     options.add_argument("--window-size=1920,1080")
-    
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=options)
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     yield driver
     driver.quit()
